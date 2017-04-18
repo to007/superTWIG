3e1f24589552b92354cb4ab52315b285543f0852
to007 <tommy@ra-go.com>
2017-04-18 13:47:42 -0400

    barely done anythingSTICK just need to do it at homeSTICK

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+import java.util.*;
+import java.io.*;
+
+public class Main
+{
+
+    public static void main(String[] args) throws IOException
+    {
+	    File raw = new File("rawDataU6.txt");
+        Scanner sc = new Scanner(raw);
+        
+    }
+}
---
+++ src/com/company/Student.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Student 
+{
+    double Q1, Q2, avg;
+    int secNum;
+    String rName;
+    public Student(double grade1, double grade2, int secretNum, String realName)
+    {
+        Q1 = grade1;
+        Q2 = grade2;
+        secNum = secretNum;
+        rName = realName;
+        avg = Q1/Q2;
+    }
+    public void avgGrades(double grade1, double grade2)
+    {
+        Q1 /= grade1;
+        Q2 /= grade2;
+    }
+}
