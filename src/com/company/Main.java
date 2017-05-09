39b49324cc8f1f0082391c740a2ab504663fceda
to007 <tommy@ra-go.com>
2017-05-09 14:30:55 -0400

    STICK

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,23 +3,48 @@ package com.company;
 import java.util.*;
 import java.io.*;
 
-public class Main
-{
 
-    public static void main(String[] args) throws IOException
-    {
+public class Main {
+
+    public static void main(String[] args) throws IOException {
 	    File raw = new File("rawDataU6.txt");
         Scanner sc = new Scanner(raw);
+        Scanner sc2 = new Scanner(raw);
+        ArrayList<Student> x = new ArrayList<>();
         String reader;
-        int count = 0;
-        double q1;
-        double q2;
+        int secnum = 1;
+        double q1 = 0;
+        double q2 = 0;
+        boolean moveOn = true, o = false;
         sc.useDelimiter(" +|\t");
         sc.nextLine();
-        while(sc.hasNext())
-        {
-            count++;
-            
+        int add = 0;
+        while(sc.hasNext()) {
+            if(moveOn && o) {
+                Student a = new Student(sc2.next(), secnum, q1, q2);
+                x.add(a);
+                secnum = sc.nextInt();
+                sc2.nextLine();
+            }
+            o = true;
+            moveOn = false;
+            reader = sc.next();
+            for (int i = 0; i < reader.length(); i++) {
+                    try {
+                        add = 0;
+                        add = Integer.parseInt(reader.substring(i,i + 1));
+                    }catch(NumberFormatException e) {
+
+                    }
+                }
+            if(add == 5){
+                q1 += .5;
+            }else{
+                q1 += (double)add;
+            }
+
+
         }
+        System.out.println(q1);
     }
 }
