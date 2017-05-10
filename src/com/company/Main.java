9ea88d2bff98737d84b989ec961a8ef211d4035c
to007 <tommy@ra-go.com>
2017-05-10 14:30:22 -0400

    STICK

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,15 +10,17 @@ public class Main {
 	    File raw = new File("rawDataU6.txt");
         Scanner sc = new Scanner(raw);
         Scanner sc2 = new Scanner(raw);
+        sc.useDelimiter("\t|,");
         ArrayList<Student> x = new ArrayList<>();
         String reader;
+        String s[];
         int secnum = 1;
         double q1 = 0;
         double q2 = 0;
         boolean moveOn = true, o = false;
-        sc.useDelimiter(" +|\t");
         sc.nextLine();
-        int add = 0;
+        sc.next();
+        double add = 0;
         while(sc.hasNext()) {
             if(moveOn && o) {
                 Student a = new Student(sc2.next(), secnum, q1, q2);
@@ -29,20 +31,16 @@ public class Main {
             o = true;
             moveOn = false;
             reader = sc.next();
-            for (int i = 0; i < reader.length(); i++) {
-                    try {
-                        add = 0;
-                        add = Integer.parseInt(reader.substring(i,i + 1));
-                    }catch(NumberFormatException e) {
+            s = reader.split(" ");
+            for (int i = 0; i < s.length; i++) {
+                try {
+                    add = Double.parseDouble(s[i]);
+                }catch(NumberFormatException e) {
 
-                    }
                 }
-            if(add == 5){
-                q1 += .5;
-            }else{
-                q1 += (double)add;
             }
-
+            q1 += add;
+            add = 0;
 
         }
         System.out.println(q1);
