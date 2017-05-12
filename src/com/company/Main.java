e3d2266086626068b67ca313bbbf60285d063c9a
to007 <tommy@ra-go.com>
2017-05-11 23:58:40 -0400

    STICK

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,49 +7,106 @@ import java.io.*;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-	    File raw = new File("rawDataU6.txt");
+        File raw = new File("rawDataU6.txt");
         File names = new File("Names.tsv");
         Scanner sc = new Scanner(raw);
-        Scanner sc2 = new Scanner(raw);
-        sc.useDelimiter("\t|,");
+        Scanner sc2 = new Scanner(names);
+        sc.useDelimiter("\t|\"");
+        sc2.useDelimiter("\t");
         ArrayList<Student> x = new ArrayList<>();
         String reader;
-        String s[];
-        int secnum = 1;
-        double q1 = 0;
-        double q2 = 0;
-        boolean moveOn = true, o = false;
+        String s[], st[];
+        int secnum = 1, num;
+        double q1;
+        double q2;
         sc.nextLine();
         sc.next();
         double add = 0;
         while(sc.hasNext()) {
-            if(moveOn && o) {
+            q1 = 0;
+            q2 = 0;
+            num = sc.nextInt();
+            if(secnum == num){
+
+            }else{
+                secnum = num;
                 Student a = new Student(sc2.next(), secnum, q1, q2);
                 x.add(a);
-                secnum = sc.nextInt();
                 sc2.nextLine();
             }
-            o = true;
-            moveOn = false;
+            sc.next();
             reader = sc.next();
-            s = reader.split(" ");
-            for (int i = 0; i < s.length; i++) {
-                try {
-                    add = Double.parseDouble(s[i]);
-                }catch(NumberFormatException e) {
+            st = reader.split(",");
+            for (int i = 0; i < st.length; i++) {
+                s = st[i].split(" ");
+                for (int j = 0; j < s.length; j++) {
+                    try {
+                        add = Double.parseDouble(s[j]);
+                    } catch (NumberFormatException e) {
 
+                    }
                 }
+                q1 += add;
+                add = 0;
             }
-            q1 += add;
-            add = 0;
+            try{
+                q1 -= sc.nextDouble()/4;
+            }catch(InputMismatchException e){
 
+            }
+            st = sc.next().split(",");
+            if(!(st[0].equals("+")))
+            {
+                sc.next();
+            }
+            reader = sc.next();
+            st = reader.split(",");
+            for (int i = 0; i < st.length; i++) {
+                s = st[i].split(" ");
+                for (int j = 0; j < s.length; j++) {
+                    try {
+                        add = Double.parseDouble(s[j]);
+                    } catch (NumberFormatException e) {
+
+                    }
+                }
+                q2 += add;
+                add = 0;
+            }
+            try{
+                q2 -= sc.nextDouble()/4;
+            }catch(InputMismatchException e){
+
+            }
+            st = sc.next().split(",");
+            if(!(st[0].equals("+")))
+            {
+                sc.next();
+            }
+
+        }
+        System.out.print("Number");
+        getTabs("Number", 6);
+        System.out.print("Name");
+        getTabs("Name", 6);
+        System.out.print("Q1 Grade");
+        getTabs("Q1 Grade", 6);
+        System.out.print("Q2 Grade");
+        getTabs("Q2 Grade", 6);
+        System.out.println("Average Grade");
+        Student.Sort(x);
+        for (int i = 0; i < x.size(); i++) {
+            x.get(i).String();
         }
 
     }
+
+
     public static void getTabs(String s, int max){
         int i = max - s.length()/4;
         for (int j = 0; j < i; j++) {
             System.out.println("\t");
         }
     }
+
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -1,5 +1,8 @@
 package com.company;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Student
 {
     public String name;
@@ -13,10 +16,41 @@ public class Student
         q1 = q1Grade;
         q2 = q2Grade;
     }
+    public String getName(){
+        return name;
+    }
     public void String(){
         System.out.print(Integer.toString(snum));
         Main.getTabs(Integer.toString(snum), 6);
         System.out.print(name);
         Main.getTabs(name, 6);
+        System.out.print(Double.toString(q1));
+        Main.getTabs(Double.toString(q1), 6);
+        System.out.print(Double.toString(q2));
+        Main.getTabs(Double.toString(q2), 6);
+        System.out.println(Double.toString(avg));
+    }
+    public static int compareTo(Student z,Student x, int y){
+        if(z.getName().substring(y,y+1).equals(x.getName().substring(y,y+1))){
+            return compareTo(z, x, y + 1);
+        }else if((int)z.getName().charAt(y) > (int)x.getName().charAt(y)){
+            return 1;
+        }
+        return -1;
+    }
+    public static ArrayList Sort(ArrayList<Student> x){
+        Student temp;
+        for (int i = 0; i < x.size(); i++) {
+            for (int j = i; j < x.size() - 1; j++) {
+                if (compareTo(x.get(j), x.get(j + 1), 0) == 1) {
+                    temp = x.get(j);
+                    x.set(j, x.get(j + 1));
+                    x.set(j + 1, temp);
+                }else if(compareTo(x.get(j), x.get(j + 1), 0) == -1 || compareTo(x.get(j), x.get(j + 1), 0) == 0){
+                    break;
+                }
+            }
+        }
+        return x;
     }
 }
